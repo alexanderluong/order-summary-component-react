fbcab93353bc4ab2b9bd35f486bd063963e5de3e
Alex Luong <v-tluong@microsoft.com>
2021-12-10 14:46:45 -0800

    Remove tailwind css

--- src/components/OrderSummaryCard/OrderSummaryCard.js
+++ src/components/OrderSummaryCard/OrderSummaryCard.js
@@ -1,12 +1,19 @@
 import { ReactComponent as HeroImage } from '../../assets/svg/illustration-hero.svg';
-
+import styles from './OrderSummaryCard.module.css';
+import Button from '../Button/Button';
 const OrderSummaryCard = () => {
   return (
-    <div className='border border-indigo-600'>
-      <HeroImage />
-      Order Summary You can now listen to millions of songs, audiobooks, and
-      podcasts on any device anywhere you like! Annual Plan $59.99/year Change
-      Proceed to Payment Cancel Order
+    <div>
+      <div>
+        <HeroImage />
+      </div>
+      <p>
+        <h1>Order Summary</h1>
+        You can now listen to millions of songs, audiobooks, and podcasts on any
+        device anywhere you like! Annual Plan $59.99/year Change
+      </p>
+      <Button>Proceed to Payment</Button>
+      <Button>Cancel Order</Button>
     </div>
   );
 };
