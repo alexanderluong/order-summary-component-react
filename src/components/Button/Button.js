632a89417e19d3a9b501f0216c26a185cf0bc27a
Alex Luong <v-tluong@microsoft.com>
2021-12-10 14:57:30 -0800

    Basic formatting for card and button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,3 +1,5 @@
+import styles from './Button.module.css';
+
 const Button = ({ children }) => {
   return <button type='button'>{children}</button>;
 };
--- src/components/OrderSummaryCard/OrderSummaryCard.js
+++ src/components/OrderSummaryCard/OrderSummaryCard.js
@@ -1,17 +1,20 @@
 import { ReactComponent as HeroImage } from '../../assets/svg/illustration-hero.svg';
 import styles from './OrderSummaryCard.module.css';
 import Button from '../Button/Button';
+
 const OrderSummaryCard = () => {
   return (
-    <div>
+    <div className={styles.card}>
       <div>
         <HeroImage />
       </div>
-      <p>
-        <h1>Order Summary</h1>
+      <h1 className={styles.h1}>Order Summary</h1>
+
+      <p className={styles.p}>
         You can now listen to millions of songs, audiobooks, and podcasts on any
-        device anywhere you like! Annual Plan $59.99/year Change
+        device anywhere you like!
       </p>
+      <p>Annual Plan $59.99/year Change</p>
       <Button>Proceed to Payment</Button>
       <Button>Cancel Order</Button>
     </div>
