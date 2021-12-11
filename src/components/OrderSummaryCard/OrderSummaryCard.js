9de5b3baeb4c5769231ed94f6c458ceb1034cb8c
Alex Luong <v-tluong@microsoft.com>
2021-12-10 17:28:16 -0800

    Css changes

--- src/components/OrderSummaryCard/OrderSummaryCard.js
+++ src/components/OrderSummaryCard/OrderSummaryCard.js
@@ -1,6 +1,7 @@
 import { ReactComponent as HeroImage } from '../../assets/svg/illustration-hero.svg';
 import styles from './OrderSummaryCard.module.css';
 import Button from '../Button/Button';
+import PlanCard from '../PlanCard/PlanCard';
 
 const OrderSummaryCard = () => {
   return (
@@ -10,16 +11,16 @@ const OrderSummaryCard = () => {
           preserveAspectRatio='xMidYMid slice'
           width='330px'
           height='150px'
-          viewBox='0 0 400 200'
+          viewBox='0 0 440 200'
         />
       </div>
       <h1 className={styles.h1}>Order Summary</h1>
 
-      <p className={styles.p}>
+      <p className={styles.description}>
         You can now listen to millions of songs, audiobooks, and podcasts on any
         device anywhere you like!
       </p>
-      <p>Annual Plan $59.99/year Change</p>
+      <PlanCard planName='Annual Plan' price='$59.99/year' />
       <Button variant='outlined'>Proceed to Payment</Button>
       <Button>Cancel Order</Button>
     </div>
--- src/components/PlanCard/PlanCard.js
+++ src/components/PlanCard/PlanCard.js
@@ -1,1 +1,22 @@
-const PlanCard = () => <div>Plan Card</div>;
+import styles from './PlanCard.module.css';
+import { ReactComponent as MusicIcon } from '../../assets/svg/icon-music.svg';
+
+const PlanCard = ({ price, planName }) => (
+  <div className={styles.card}>
+    <MusicIcon
+      preserveAspectRatio='none'
+      height='42'
+      width='42'
+      viewBox='0 0 50 50'
+    />
+    <span class={`flex-column ${styles.plan}`}>
+      <span>{planName}</span>
+      <span>{price}</span>
+    </span>
+    <a className={styles.link} href='#'>
+      Change
+    </a>
+  </div>
+);
+
+export default PlanCard;
