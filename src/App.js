9dc90cc55b2e1a736da9d2309f00f00d266b671d
alexanderluong <alexander.luong@hotmail.com>
2021-12-13 00:12:52 -0800

    Start background work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+
+import { ReactComponent as DesktopBackgroundSvg } from './assets/svg/pattern-background-desktop.svg';
+import { ReactComponent as MobileBackgroundSvg } from './assets/svg/pattern-background-mobile.svg';
 import OrderSummaryCard from './components/OrderSummaryCard/OrderSummaryCard';
 
 function App() {
   return (
     <div className='App'>
+      <div>
+        <DesktopBackgroundSvg
+          className='desktopBackgroundSvg'
+          viewBox='0 0 1440 600'
+          height='100%'
+          width='100%'
+        />
+      </div>
       <OrderSummaryCard />
+      {/* <MobileBackgroundSvg className='mobileBackgroundSvg' /> */}
+
       <div class='attribution'>
         Challenge by{' '}
         <a
