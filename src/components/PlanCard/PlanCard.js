636e95c935a7653965920566d439d3c19801bd23
alexanderluong <alexander.luong@hotmail.com>
2021-12-12 23:12:51 -0800

    Fix formatting for plan card

--- src/components/PlanCard/PlanCard.js
+++ src/components/PlanCard/PlanCard.js
@@ -1,17 +1,19 @@
-import styles from './PlanCard.module.css';
 import { ReactComponent as MusicIcon } from '../../assets/svg/icon-music.svg';
+import styles from './PlanCard.module.css';
 
 const PlanCard = ({ price, planName }) => (
   <div className={styles.card}>
     <MusicIcon
       preserveAspectRatio='none'
-      height='42'
-      width='42'
+      height='46'
+      width='46'
       viewBox='0 0 50 50'
     />
     <span class={`flex-column ${styles.plan}`}>
-      <span>{planName}</span>
-      <span>{price}</span>
+      <p>
+        <b>{planName}</b>
+      </p>
+      <p>{price}</p>
     </span>
     <a className={styles.link} href='#'>
       Change
